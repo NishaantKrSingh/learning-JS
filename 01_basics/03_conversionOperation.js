43ae0cd58c0ebc4b7e35bbbf53203de2573617cc
nishaantkrsingh <nishaantkrsingh@gmail.com>
2023-12-06 13:06:55 +0530

    Conversion Operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "Nishant"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN (Not a Number)
+//NaN is also considered as a number which checked with typeof
+//true=> 1; false=> 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
