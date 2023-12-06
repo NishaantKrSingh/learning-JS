88f86b24764cfd22c0d902f36649a727e6c6be20
nishaantkrsingh <nishaantkrsingh@gmail.com>
2023-12-06 12:37:31 +0530

    added dataTypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,4 +1,18 @@
 const accountID = 144553  //conat variable can't be changed
 let accountEmail = "email@google.com"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+accountCity = "Jaipur" //place can be reserved without defining but is not recommended
+let accountState;
+// accountID = 2 // not allowed because const variable
+
+accountEmail = "mail@mail.com"
+accountPassword = "54321"
+accountCity = "bangaluru"
+
+/*
+Prefer not to use var 
+beacuse of issue in block scope and functional scope
+*/
+
+console.log(accountID);
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("") // can't be used because we're using node js and not in browser
+
+console.log(3
+    +
+    3) // code readibility should be high so this is not a good example
+
+let name = "Nishant"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to the power 53
+// bigint => generally used to deal with very large numbers
+// string => is surrounded with ""
+// boolean => true/false
+// null => standalone value
+// undefined => is something that is not defined
+// symbol => unique
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+// is considered as a flaw of the language
